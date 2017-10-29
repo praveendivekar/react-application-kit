cd00226d143b734521f8a2fb0bd5eeaa9c7fbad2
praveendivekar <praveendivekar@gmail.com>
2017-10-29 22:53:36 +0530

    Routing, states and props in React

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import ShowCard from './ShowCard';
-import preload from '../data.json';
+import React, { Component } from "react";
+import ShowCard from "./ShowCard";
+import preload from "../data.json";
 
-const Search = () => (
-    <div className='search'>
-    <div>
-        {preload.shows.map(show =>
-            <ShowCard {...show} key={show.imdbID}/>
-        )}
-    </div>
-    </div>
-);
+class Search extends Component {
+  state = {
+    seachTerm: ''
+  };
+  handleSearchTermEvent = event => {
+    this.setState({ seachTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>Search Video</h1>
+          <input
+            onChange={this.handleSearchTermEvent}
+            type="text"
+            placeholder="Search"
+            value={this.state.seachTerm}
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+              (`${show.title} ${show.description}`.toUpperCase()
+                  .indexOf(this.state.seachTerm.toUpperCase()) >= 0)
+            )
+            .map(show => <ShowCard {...show} key={show.imdbID} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
