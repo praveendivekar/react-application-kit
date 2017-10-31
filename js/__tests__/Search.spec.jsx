6d682eac67a7bb25cd7ff47bd0204feea52ca6eb
praveendivekar <praveendivekar@gmail.com>
2017-10-31 16:58:28 +0530

    Hot module replacement

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,4 +1,25 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Search from '../Search';
+import { shallow } from 'enzyme';
+import ShowCard from '../ShowCard';
+import preload from '../../data.json';
 
+test('Search renders correctly', () => {
+    const component = shallow(<Search />)
+    expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct amount of shows', () => {
+    const component = shallow(<Search />);
+    expect(preload.shows.length).toEqual(component.find(ShowCard).length);
+});
+
+xtest('Search should render correct amount of shows based on search term', () => {
+    const searchWord = 'black';
+    const component = shallow(<Search />);
+    component.find('input').simulate('change', {target:{value:searchWord}});
+    const showCount = preload.shows.filter(show =>
+      `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase() >= 0 )
+    ).length;
+    expect(component.find(showCard).length).toEqual(showCount.length);
+});
